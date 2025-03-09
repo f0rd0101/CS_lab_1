69c4bf60759d7ac33b9cce66955d3794748d9675
clover <clover@localhost.localdomain>
2025-03-10 01:49:00 +0200

    changed logic

--- task2.c
+++ task2.c
@@ -3,14 +3,14 @@
 #define N 5
 
 int main() {
-    int mas[N], i, minIndex = 0, maxIndex = 0, product = 1, sumNeg = 0;
+    int mas[N], i, minIndex = 0, maxIndex = 0, product = 1, s = 0;
     
     puts("Enter array numbers:");
     for (i = 0; i < N; i++) {
         scanf("%d", &mas[i]);
         if (mas[i] < mas[minIndex]) minIndex = i;
         if (mas[i] > mas[maxIndex]) maxIndex = i;
-        if (mas[i] < 0) sumNeg += mas[i];
+        if (mas[i] < 0) s += mas[i];
     }
     
     if (minIndex > maxIndex) {
@@ -21,7 +21,7 @@ int main() {
     
     for (i = minIndex + 1; i < maxIndex; i++) product *= mas[i];
     
-    printf("Non-positive sum: %d\n", sumNeg);
+    printf("Non-positive sum: %d\n", s);
     printf("Product of elements between min and max: %d\n", maxIndex - minIndex > 1 ? product : 0);
     return 0;
 }
